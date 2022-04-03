dc24cd7aface3fd6094daeba882278e0eb0cf2d3
Paul Wilson <paulanthonywilson@gmail.com>
2022-04-03 20:56:39 +0100

    new nerves

---
+++ apps/fw/lib/fw/application.ex
@@ -0,0 +1,44 @@
+defmodule Fw.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Fw.Supervisor]
+
+    children =
+      [
+        # Children for all targets
+        # Starts a worker by calling: Fw.Worker.start_link(arg)
+        # {Fw.Worker, arg},
+      ] ++ children(target())
+
+    Supervisor.start_link(children, opts)
+  end
+
+  # List all child processes to be supervised
+  def children(:host) do
+    [
+      # Children that only run on the host
+      # Starts a worker by calling: Fw.Worker.start_link(arg)
+      # {Fw.Worker, arg},
+    ]
+  end
+
+  def children(_target) do
+    [
+      # Children for all targets except host
+      # Starts a worker by calling: Fw.Worker.start_link(arg)
+      # {Fw.Worker, arg},
+    ]
+  end
+
+  def target() do
+    Application.get_env(:fw, :target)
+  end
+end
