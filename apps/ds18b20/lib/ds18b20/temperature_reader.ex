9d365444da2a433b6f73f2523b5d40ae49ddadf3
Paul Wilson <paulanthonywilson@gmail.com>
2022-04-06 00:34:36 +0100

    Parsing one wire temperature

--- apps/ds18b20/lib/ds18b20/temperature_reader.ex
+++ apps/ds18b20/lib/ds18b20/temperature_reader.ex
@@ -5,6 +5,8 @@ defmodule Ds18b20.TemperatureReader do
 
   @devices_base "/sys/bus/w1/devices/"
 
+  @temperature_regex ~r/crc=\w\w\s*(?<crc>\w+)$.*t=(?<t>\d+)$/sm
+
   @doc """
   Finds the device file
   """
@@ -23,5 +25,31 @@ defmodule Ds18b20.TemperatureReader do
       [dir] -> {:ok, Path.join([base_dir, dir, "w1_slave"])}
       _ -> {:error, :multiple_devices}
     end
+  end
+
+  def read_temperature(device_file) do
+    device_file
+    |> File.read()
+    |> parse_file()
+  end
+
+  defp parse_file({:ok, body}) do
+    case Regex.named_captures(@temperature_regex, body) do
+      %{"t" => t, "crc" => "YES"}  ->
+        {:ok, to_celsius(t)}
+        %{"t" => _} ->
+          {:error, :crc_fail}
+
+      _ ->
+        {:error, :bad_data}
+    end
+  end
+
+  defp parse_file(err), do: err
+
+  defp to_celsius(t) do
+    t
+    |> Decimal.new()
+    |> Decimal.div(1000)
   end
 end
--- apps/ds18b20/test/ds18b20/temperature_reader_test.exs
+++ apps/ds18b20/test/ds18b20/temperature_reader_test.exs
@@ -37,4 +37,45 @@ defmodule Ds18b20.TemperatureReaderTest do
                TemperatureReader.device_file(base_dir)
     end
   end
+
+  describe "reading the device file" do
+    setup %{base_dir: base_dir} do
+      dir = Path.join(base_dir, "28-abcd")
+      device_file = Path.join(dir, "w1-slave")
+      File.mkdir!(dir)
+      {:ok, device_file: device_file}
+    end
+
+    test "error if the file does not exist", %{device_file: device_file} do
+      assert {:error, :enoent} == TemperatureReader.read_temperature(device_file)
+    end
+
+    test "error if the file is nonsense", %{device_file: device_file} do
+      File.write!(device_file, "blah de blah")
+      assert {:error, :bad_data} == TemperatureReader.read_temperature(device_file)
+    end
+
+    test "gets temperature from valid file", %{device_file: device_file} do
+      content = """
+      e3 00 4b 46 7f ff 0c 10 7e : crc=7e YES
+      e3 00 4b 46 7f ff 0c 10 7e t=14187
+      """
+
+      File.write!(device_file, content)
+
+      assert {:ok, %Decimal{} = temperature} = TemperatureReader.read_temperature(device_file)
+      assert Decimal.equal?("14.187", temperature)
+    end
+
+    test "error if crc is not valid", %{device_file: device_file} do
+      content = """
+      e3 00 4b 46 7f ff 0c 10 7e : crc=7e NO
+      e3 00 4b 46 7f ff 0c 10 7e t=14187
+      """
+
+      File.write!(device_file, content)
+
+      assert {:error, :crc_fail} = TemperatureReader.read_temperature(device_file)
+    end
+  end
 end
