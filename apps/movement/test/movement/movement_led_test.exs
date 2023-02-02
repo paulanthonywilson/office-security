88b4e4b291142c1026d30f23bab8cde500649006
Paul Wilson <paulanthonywilson@gmail.com>
2023-02-02 12:41:15 +0000

    Use actual process in led test

--- apps/movement/test/movement/movement_led_test.exs
+++ apps/movement/test/movement/movement_led_test.exs
@@ -6,19 +6,27 @@ defmodule Movement.MovementLedTest do
   @a_date_time ~U[2022-11-10 09:08:07Z]
 
   setup do
-    {:ok, led} = GPIO.open(63, :output)
-    on_exit(fn -> GPIO.close(led) end)
-    {:ok, led: led}
+    unique_name = self() |> inspect() |> String.to_atom()
+    {:ok, pid} = MovementLed.start_link(pin: 0, name: unique_name)
+    {:ok, outpin} = GPIO.open(1, :output)
+
+    {:ok, pid: pid, outpin: outpin}
   end
 
-  test "turns the light on", %{led: led} do
-    MovementLed.handle_info({Movement.Sensor, :movement_detected, @a_date_time}, %{led: led})
-    assert 1 == GPIO.read(led)
+  test "turns the light on", %{outpin: outpin, pid: pid} do
+    send(pid, {Movement.Sensor, :movement_detected, @a_date_time})
+    process_message_queue(pid)
+    assert 1 == GPIO.read(outpin)
   end
 
-  test "turns the light off", %{led: led} do
-    GPIO.write(led, 1)
-    MovementLed.handle_info({Movement.Sensor, :movement_stopped, @a_date_time}, %{led: led})
-    assert 0 == GPIO.read(led)
+  test "turns the light off", %{outpin: outpin, pid: pid} do
+    send(pid, {Movement.Sensor, :movement_detected, @a_date_time})
+    send(pid, {Movement.Sensor, :movement_stopped, @a_date_time})
+    process_message_queue(pid)
+    assert 0 == GPIO.read(outpin)
+  end
+
+  defp process_message_queue(pid) do
+    :sys.get_state(pid)
   end
 end
